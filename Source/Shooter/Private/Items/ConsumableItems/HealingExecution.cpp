6315a7df1caa53592d3a25c92b888084c61af32d
seocom <wjddnjs6520@naver.com>
2025-08-01 21:54:20 +0900

    Add: 힐링 아이템 기능 구현 (HealingItem, HealingItemAttributeSet, GA, GE, Execution)
    HealingItem
    - ConsumableItemBase를 상속받은 힐 아이템 클래스입니다.
    - PickupItem 함수를 오버라이드하여 HealingItemAttributeSet을 할당하도록 하였습니다.
    
    HealingItemAttributeSet
    - 힐 아이템의 회복량이 들어있는 AttributeSet 입니다.
    
    HealingExecution
    - 시전자의 ASC에서 CurrentHealth와 힐아이템의 HealAmount를 불러와 힐 아이템의 회복량만큼 증가시키는 로직을 가지고 있습니다.
    
    GA_UserHealingItem
    - Activate Ability 시 캐릭터의 인벤토리의 아이템 개수를 줄이고 GE_HealingItemEffect를 실행합니다.
    
    GE_HealingItemEffect
    - HealingExectuion을 할당 받아 해당 로직을 실행합니다.

---
+++ Source/Shooter/Private/Items/ConsumableItems/HealingExecution.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/ConsumableItems/HealingExecution.h"
+
+UHealingExecution::UHealingExecution()
+{
+}
+
+void UHealingExecution::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+	if (!TargetASC) return;
+
+	const UShooterAttributeSet* TargetAttrSet = TargetASC->GetSet<UShooterAttributeSet>();
+	if (!TargetAttrSet) return;
+
+	const UHealingItemAttributeSet* HealAttrSet = TargetASC->GetSet<UHealingItemAttributeSet>();
+	if (!HealAttrSet) return;
+
+	float HealAmount = HealAttrSet->GetHealAmount();
+
+	// 맞은 ShooterAttributeSet의 Current Health 변수 만큼 Additive(더하기)
+	FGameplayModifierEvaluatedData EvalData(
+		UShooterAttributeSet::GetCurrentHealthAttribute(),
+		EGameplayModOp::Additive,
+		HealAmount
+	);
+
+	OutExecutionOutput.AddOutputModifier(EvalData);
+}
---
+++ Source/Shooter/Private/Items/ConsumableItems/HealingItem.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/ConsumableItems/HealingItem.h"
+#include "AbilitySystem/ShooterAbilitySystemComponent.h"
+#include "Items/ConsumableItems/HealingItemAttributeSet.h"
+#include "Characters/ShooterCharacter.h"
+
+AHealingItem::AHealingItem()
+{
+
+}
+
+void AHealingItem::PickupItem(AShooterCharacter* Picker)
+{
+	Super::PickupItem(Picker);
+
+	UShooterAbilitySystemComponent* PickerASC = Picker->GetShooterAbilitySystemComponent();
+	if (!PickerASC) return;
+
+	if (HealingItemAttributeSetClass)
+	{
+		UHealingItemAttributeSet* HealingItemAttrSet = NewObject<UHealingItemAttributeSet>(Picker, HealingItemAttributeSetClass);
+		
+		PickerASC->AddSpawnedAttribute(HealingItemAttrSet);
+
+		UE_LOG(LogTemp, Warning, TEXT("HealingItemAttrSet Give Owner"));
+	}
+}
