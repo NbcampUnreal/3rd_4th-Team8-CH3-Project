962e4d92a2c2099279a736dc0adcb9e582fd5bc2
chan <byby3534@gmail.com>
2025-08-01 14:02:18 +0900

    feat: Enemy 체력변화 로직 및 이빌리티 적용 로직 추가

--- Source/Shooter/Public/Characters/ShooterEnemyCharacter.h
+++ Source/Shooter/Public/Characters/ShooterEnemyCharacter.h
@@ -4,13 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "Characters/ShooterBaseCharacter.h"
+#include "GameplayEffectTypes.h"
 #include "ShooterEnemyCharacter.generated.h"
 
+
+class UDataAsset_InputConfig;
+class AAIController;
 /**
- * 
+ *
  */
 UCLASS()
 class SHOOTER_API AShooterEnemyCharacter : public AShooterBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AShooterEnemyCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnHealthAttributeChanged(const FOnAttributeChangeData& Data);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "GAS")
+	void OnHealthChanged(float OldValue, float NewValue);
 };
