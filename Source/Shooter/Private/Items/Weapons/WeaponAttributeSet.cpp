2cc5d74f27fc2bca13767a42028f5c41aabe883e
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-08-08 11:10:28 +0900

    Add Rampage Assets 및 Weaponattributeset Logic 업데이트
    애니메이션, 능력, 게임 플레이 효과 및 청사진을 포함하여 새로운 보스 Rampage에 대한 여러 자산을 도입했습니다. Debug 인쇄 및 형식 변경으로 AMMO 업데이트를 처리하는 기능을 유지하면서 'WeaponAttributesET'을 업데이트했습니다.

--- Source/Shooter/Private/Items/Weapons/WeaponAttributeSet.cpp
+++ Source/Shooter/Private/Items/Weapons/WeaponAttributeSet.cpp
@@ -4,35 +4,36 @@
 #include "Items/Weapons/WeaponAttributeSet.h"
 #include "GameplayEffectExtension.h"
 #include "Components/UI/ShooterUIComponent.h"
+#include "Shooter/ShooterDebugHelper.h"
 
 UWeaponAttributeSet::UWeaponAttributeSet()
 {
-    InitMaxAmmo(30.f);
-    InitCurrentAmmo(GetMaxAmmo());
+	InitMaxAmmo(30.f);
+	InitCurrentAmmo(GetMaxAmmo());
 }
 
 void UWeaponAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
 {
-    if (Data.EvaluatedData.Attribute == GetCurrentAmmoAttribute())
-    {
-        float NewAmmo = GetCurrentAmmo();
+	Debug::Print("test");
+	if (Data.EvaluatedData.Attribute == GetCurrentAmmoAttribute())
+	{
+		float NewAmmo = GetCurrentAmmo();
 
-        // Actor 소유자 가져오기
-        AActor* OwnerActor = GetOwningActor();
-        if (!OwnerActor)
-        {
-            return;
-        }
+		// Actor 소유자 가져오기
+		AActor* OwnerActor = GetOwningActor();
+		if (!OwnerActor)
+		{
+			return;
+		}
 
-        // UI 컴포넌트 가져오기
-        UShooterUIComponent* UIComponent = OwnerActor->FindComponentByClass<UShooterUIComponent>();
-        if (!UIComponent)
-        {
-            return;
-        }
+		// UI 컴포넌트 가져오기
+		UShooterUIComponent* UIComponent = OwnerActor->FindComponentByClass<UShooterUIComponent>();
+		if (!UIComponent)
+		{
+			return;
+		}
 
-        // 총알 변경 알림 보내기
-        UIComponent->HandleCurrentAmmoChanged(NewAmmo);
-    }
+		// 총알 변경 알림 보내기
+		UIComponent->HandleCurrentAmmoChanged(NewAmmo);
+	}
 }
-
